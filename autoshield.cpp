c40c6fe41686b3a91a9dc5682c7c0cf52850ccbd
ALEXANDRE MARTINS <86690337+alexnadreMartins@users.noreply.github.com>
2023-07-17 12:56:01 -0300

    Update autoshield.cpp

--- autoshield.cpp
+++ autoshield.cpp
@@ -1,1 +1,67 @@
+// Ainda é apenas uma simples conexão mas ainda preciso implementar diagnosticos por dispositivos, 
+//ainda tenho muitos estudos pela frente para concluir pelo menos a parte de analise, mitigação e contrução de um ids efetivo. 
+#include <iostream>
+#include <cstring>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <linux/can.h>
+#include <linux/can/raw.h>
+
+
+
+struct DeviceStatus {
+    bool isWorking;
+    // Outros campos relevantes para o status do dispositivo
+};
+
+DeviceStatus checkDeviceStatus(/* Parâmetros necessários para o diagnóstico */) {
+    DeviceStatus status;
+    // Implemente a lógica de diagnóstico específica para cada dispositivo
+    // Atribua o valor apropriado ao campo "isWorking" e outros campos relevantes
+
+    return status;
+}
+
+void printDeviceStatus(const DeviceStatus& status, const std::string& deviceName) {
+    std::cout << "Status do dispositivo " << deviceName << ": ";
+    if (status.isWorking) {
+        std::cout << "Funcionando corretamente" << std::endl;
+    } else {
+        std::cout << "Com falha" << std::endl;
+    }
+    // Outras informações relevantes podem ser impressas aqui
+}
+
+int main() {
+    int sock;
+    struct sockaddr_can addr;
+    struct ifreq ifr;
+
+    // Abrir o socket CAN
+    sock = socket(PF_CAN, SOCK_RAW, CAN_RAW);
+    if (sock < 0) {
+        std::cerr << "Erro ao abrir o socket CAN" << std::endl;
+        return -1;
+    }
+
+    // Configurar a interface CAN
+    strcpy(ifr.ifr_name, "can0"); // Substitua "can0" pelo nome da sua interface CAN
+    ioctl(sock, SIOCGIFINDEX, &ifr);
+
+    addr.can_family = AF_CAN;
+    addr.can_ifindex = ifr.ifr_ifindex;
+
+    // Associar o socket à interface CAN
+    if (bind(sock, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
+        std::cerr << "Erro ao associar o socket à interface CAN" << std::endl;
+        close(sock);
+        return -1;
+    }
+
+
+    // Fechar o socket CAN
+    close(sock);
+
+    return 0;
+}
 
